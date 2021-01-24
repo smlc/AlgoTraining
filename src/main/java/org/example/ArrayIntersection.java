bba6061b19286174c545d0a238e463466d0fdd7c
smlc <siesa.marlon@hotmail.fr>
2021-01-24 15:50:58 +0000

    array intersection

--- src/main/java/org/example/ArrayIntersection.java
+++ src/main/java/org/example/ArrayIntersection.java
@@ -1,28 +1,39 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ArrayIntersection {
 
-	static int[] intersection(int[] arr1, int[] arr2) {
+	public static int[] intersection(int[] nums1, int[] nums2) {
 
-		List<Integer> intersection = new ArrayList<>();
+		Set<Integer> setOne = new HashSet<>();
+		for (int n : nums1) setOne.add(n);
+		Set<Integer> setTwo = new HashSet<>();
+		for (int n : nums2) setTwo.add(n);
 
-		int i = 0 , j = 0;
 
-		while(i < arr1.length & j < arr2.length){
+		if(setOne.size() < setTwo.size()) return intersectionSet(setOne, setTwo);
+		else  return intersectionSet(setTwo, setOne);
+	}
 
-		}
+	public static int[] intersectionSet(Set<Integer> set1, Set<Integer> set2) {
+		int [] output = new int[set1.size()];
+		int idx = 0;
+		for (int s : set1)
+			if (set2.contains(s)) output[idx++] = s;
 
-		int[] result = new int[intersection.size()];
-
-		return result;
+		return Arrays.copyOf(output, idx);
 	}
 	public static void main( String[] args )
 	{
 
 		int arr1[] = {1, 2, 3, 3, 4, 5, 5, 6};
-		int arr3[] = {3, 3, 5};
+		int arr2[] = {3, 3, 5};
+
+		int result[] = intersection(arr1, arr2);
+
+		for (int n : result) System.out.print(n + " ");
 	}
 }
